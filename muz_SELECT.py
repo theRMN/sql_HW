4e2247af29b6c28077f65e01bcd78948a937b2c2
theRMN <roman.vassev@gmail.com>
2021-04-18 22:43:48 +0600

    add request 1

--- muz_SELECT.py
+++ muz_SELECT.py
@@ -1,30 +1,38 @@
 import sqlalchemy
+from pprint import pprint
 
 db = 'postgresql://muz:muz@localhost:5432/muz'
 engine = sqlalchemy.create_engine(db)
 connection = engine.connect()
 
-al_year = connection.execute("""SELECT name, year_of_issue FROM albums WHERE year_of_issue = 2011;
-                            """).fetchall()
-print(al_year)
+# al_year = connection.execute("""SELECT name, year_of_issue FROM albums WHERE year_of_issue = 2011;
+#                             """).fetchall()
+# print(al_year)
+#
+# tr_dur = connection.execute("""SELECT name, duration FROM tracks ORDER BY duration DESC;
+#                             """).fetchone()
+# print(tr_dur)
+#
+# tr_dur2 = connection.execute("""SELECT name, duration FROM tracks WHERE duration >= 3.5 ORDER BY duration;
+#                             """).fetchall()
+# print(tr_dur2)
+#
+# col_year = connection.execute("""SELECT name, year_of_issue FROM collections WHERE year_of_issue
+#                             BETWEEN 2018 AND 2020;
+#                             """).fetchall()
+# print(col_year)
+#
+# my_word = connection.execute("""SELECT name FROM tracks WHERE name LIKE '%%_my_%%' OR name LIKE '%%_My_%%';
+#                             """).fetchall()
+# print(my_word)
+#
+# one_word = connection.execute("""SELECT name FROM tracks WHERE (LENGTH(name) - LENGTH(replace(name, ' ', ''))) = 0;
+#                             """).fetchall()
+# print(one_word)
 
-tr_dur = connection.execute("""SELECT name, duration FROM tracks ORDER BY duration DESC;
-                            """).fetchone()
-print(tr_dur)
+ga_count = connection.execute("""SELECT name, COUNT(artist_id) FROM genres g
+                        LEFT JOIN artist_genres_albums aga ON aga.genres_id = g.id_genres
+                        GROUP BY g.name;
+                        """).fetchall()
 
-tr_dur2 = connection.execute("""SELECT name, duration FROM tracks WHERE duration >= 3.5 ORDER BY duration;
-                            """).fetchall()
-print(tr_dur2)
-
-col_year = connection.execute("""SELECT name, year_of_issue FROM collections WHERE year_of_issue
-                            BETWEEN 2018 AND 2020; 
-                            """).fetchall()
-print(col_year)
-
-my_word = connection.execute("""SELECT name FROM tracks WHERE name LIKE '%%_my_%%' OR name LIKE '%%_My_%%';
-                            """).fetchall()
-print(my_word)
-
-one_word = connection.execute("""SELECT name FROM tracks WHERE (LENGTH(name) - LENGTH(replace(name, ' ', ''))) = 0;
-                            """).fetchall()
-print(one_word)
+pprint(ga_count)
